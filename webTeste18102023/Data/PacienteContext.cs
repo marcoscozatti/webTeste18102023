0005e91968c8da794b9067d3c3a3d19280b4406e
marcoscozatti <marcoscozatti@gmail.com>
2023-10-24 15:19:29 -0300

    Metodo Scaffold adicionado

---
+++ webTeste18102023/Data/PacienteContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using webTeste18102023.Models;
+
+namespace webTeste18102023.Data
+{
+    public class PacienteContext : DbContext
+    {
+        public PacienteContext (DbContextOptions<PacienteContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<webTeste18102023.Models.Paciente> Paciente { get; set; }
+    }
+}
